e12515833d0b87ed3723713543976dda750d5faa
alex3653 <74103537+alex3653@users.noreply.github.com>
2020-11-09 16:57:44 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+            public static void main(String[] args) {
+                String mas1[] = {"Ivan", "Vlad", "Vera"};
+                int mas2[] = {18, 19, 20};
+                for (int i = 0; i < mas1.length; i++)
+                {
+                    System.out.println(mas1[i] + " " + mas2[i]);
+                }
+            }
+
+        }
+
+
+
